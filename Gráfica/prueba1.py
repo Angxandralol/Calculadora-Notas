7aaf81c267cba83d64fe3201a3a4bebac50d8504
angxandralol <aalexandramv@gmail.com>
2022-09-15 20:45:45 -0400

    Actualizacion de archivos

---
+++ Gráfica/prueba1.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+raiz = Tk()
+raiz.title("Ventana de pruebas")
+raiz.resizable(1,0)
+#raiz.iconbitmap("ruta del archivo") Cambia el icono de la ventana
+raiz.geometry("400x200")
+raiz.config(bg="pink")
+
+
+raiz.mainloop()
+
